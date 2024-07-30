74a10f559ff116c3af2b7be360172954560123b8
Owain <88115597+OwainYT@users.noreply.github.com>
2024-07-30 15:59:57 +0100

    Added more features; Add Timestamps: Include timestamps for each message. Popup Notifications: Implement popup notifications when new messages arrive. Typing Notifications: Show a "user is typing..." message. Username Management: Save the username in a cookie and allow users to change it.

--- server.js
+++ server.js
@@ -11,15 +11,35 @@ const io = socketIo(server);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const onlineUsers = {};
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    // When a user sends their username
+    socket.on('set username', (username) => {
+        socket.username = username;
+        onlineUsers[socket.id] = username;
+        io.emit('update users', Object.values(onlineUsers));
+    });
+
     socket.on('chat message', (msg) => {
+        msg.timestamp = new Date().toLocaleTimeString();
         io.emit('chat message', msg);
+    });
+
+    socket.on('typing', (username) => {
+        socket.broadcast.emit('typing', username);
+    });
+
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing');
     });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
+        delete onlineUsers[socket.id];
+        io.emit('update users', Object.values(onlineUsers));
     });
 });
 
